0696a936f51fa0c0c7466bafdba5038a7b15347b
ariyudhaprawira <ariyudhaprawira@gmail.com>
2017-05-04 04:18:32 +0700

    Template halaman depan

---
+++ belajar/tabel.php
@@ -0,0 +1,12 @@
+<table align="center" border="1">
+		<tr align="center">
+			<td>NO</th>
+			<td>NIM</th>
+			<td>Nama</td>
+		</tr>
+		<tr align="center">
+			<td>1</th>
+			<td>H1101151036</th>
+			<td>Ari Yudha Prawira</td>		
+		</tr>
+</table>
